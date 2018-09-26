d3dc6e5265fbb1f852392ebd7d7f4bac46b87686
Karissa <kmckendall@gmail.com>
2018-09-26 18:30:07 -0400

    working on the page scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,16 +10,16 @@ const navbar = (props) => {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                 <li className="nav-item active">
-                    <a className="nav-link" href="#">About Me<span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="#aboutme">About Me<span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Skills/Technologies</a>
+                    <a className="nav-link" href="#skills">Skills/Technologies</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Portfolio</a>
+                    <a className="nav-link" href="#portfolio">Portfolio</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Contact</a>
+                    <a className="nav-link disabled" href="#contact">Contact</a>
                 </li>
                 </ul>
             </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ScrollableAnchor, {configureAnchors, goToTop} from 'react-scrollable-anchor';
 import './App.css';
 import Navbar from '../components/Navbar/Navbar';
 import Jumbotron from '../components/Jumbotron/Jumbotron';
@@ -8,17 +9,32 @@ import Portfolio from '../components/Portfolio/Portfolio';
 import Contact from '../components/Contact/Contact';
 import Footer from '../components/Footer/Footer';
 
-class App extends Component {
-  render() {
+class App extends Component { 
+  componentWillMount() {
+    configureAnchors({})
+  } 
+  render() {    
     return (
       <div>
         <Navbar />
           <div className='container'>
-            <Jumbotron />
-            <AboutMe />
-            <Skills />
-            <Portfolio />
-            <Contact />
+            <ScrollableAnchor id={'jumbotron'}>
+              <Jumbotron />
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'aboutme'}>
+              <div>
+                <AboutMe />
+              </div>
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'skills'}>
+              <Skills />
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'portfolio'}>
+              <Portfolio />
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'contact'}>
+              <Contact />
+            </ScrollableAnchor>
             <Footer />
           </div>
       </div>
